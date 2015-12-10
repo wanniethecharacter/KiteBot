fc16ba06adbf31025a5ac6792222954345b3c216
LassieME <sindre_langhus@msn.com>
2015-12-10 23:17:47 +0100

    Added settings

---
+++ ConsoleApplication1/KiteDunk.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiteBot
+{
+	class KiteDunk
+	{
+		private static string kiteDunksApi = "https://spreadsheets.google.com/feeds/list/11024r_0u5Mu-dLFd-R9lt8VzOYXWgKX1I5JamHJd8S4/od6/public/values?hl=en_US&&alt=json";
+		private string[] kiteDunks;
+		
+		KiteDunk()
+		{
+			string response;
+			using (WebClient client = new WebClient())
+			{
+				response = client.DownloadString(kiteDunksApi);
+			}
+
+		}
+	}
+}
