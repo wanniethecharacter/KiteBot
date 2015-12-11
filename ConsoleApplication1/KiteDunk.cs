c93a1b3c38e9bf91a7d991a5686f9e220755cd1b
LassieME <sindre_langhus@msn.com>
2015-12-11 13:55:08 +0100

    Moved KiteDunks into its own class

--- ConsoleApplication1/KiteDunk.cs
+++ ConsoleApplication1/KiteDunk.cs
@@ -1,28 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.IO;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace KiteBot
 {
-	class KiteDunk
+	internal class KiteDunk
 	{
-		private static string kiteDunksApi = "https://spreadsheets.google.com/feeds/list/11024r_0u5Mu-dLFd-R9lt8VzOYXWgKX1I5JamHJd8S4/od6/public/values?hl=en_US&&alt=json";
-		private string[] kiteDunks;
-		
-		KiteDunk()
+		public static string[] _kiteDunks;
+		public static Random Rnd;
+
+		public KiteDunk()
+		{
+			_kiteDunks = System.IO.File.ReadAllLines(@"C:\Users\sindr\Documents\visual studio 2013\Projects\ConsoleApplication1\ConsoleApplication1\KiteDunks3.txt");
+			Rnd = new Random(System.DateTime.Now.Millisecond);
+		}
+
+		public string GetRandomKiteDunk()
+		{
+			var i = Rnd.Next(_kiteDunks.Length / 2) * 2;
+			return "\"" + _kiteDunks[i + 1] + "\" - " + _kiteDunks[i];
+		}
+		private void UpdateKiteDunks()
 		{
 			string response;
-			using (WebClient client = new WebClient())
+			using (var client = new WebClient())
 			{
-				response = client.DownloadString(kiteDunksApi);
+				response = client.DownloadString("https://spreadsheets.google.com/feeds/list/11024r_0u5Mu-dLFd-R9lt8VzOYXWgKX1I5JamHJd8S4/od6/public/values?hl=en_US&&alt=json");
 			}
-			dynamic o = JsonConvert.DeserializeObject(response);
-			o.
+			var regex1 = new Regex("\"gsx\\$name\":{\"\\$t\":\".+?\"}|\"gsx\\$quote\":{\"\\$t\":\".+?\"}}", RegexOptions.Singleline);
+			var matches = regex1.Matches(response);
+			var i = 0;
+			var kiteDunks = new string[matches.Count];
+			foreach(var match in matches)
+			{
+				var s = match.ToString().Replace("\"gsx$name\":{\"$t\":","").Replace("gsx$quote\":{\"$t\":","").Replace("}","").Replace("\"","");
+				kiteDunks[i] = s;
+				i++;
+			}
+			File.WriteAllLines(path: "C:\\Users\\sindr\\Documents\\visual studio 2013\\Projects\\ConsoleApplication1\\ConsoleApplication1\\KiteDunks2.txt", contents: kiteDunks);
 		}
 	}
 }
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,99 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-using System.Text;
-using System.Threading;
-using System.Xml;
-using System.Net.Http;
-using System.Text.RegularExpressions;
-
 
 namespace ConsoleApplication1
 {
     class Program
     {
-		public static Discord.DiscordClient client;
-		public static long id = 85842104034541568;
-		public static string kiteDunksApi = "https://spreadsheets.google.com/feeds/list/11024r_0u5Mu-dLFd-R9lt8VzOYXWgKX1I5JamHJd8S4/od6/public/values?hl=en_US&&alt=json";
-		public static string[] kiteDunks;
-		public static Random rnd;
+		public static Discord.DiscordClient Client;
 
 		static void Main(string[] args)
         {
-            client = new Discord.DiscordClient();
-			GetKiteDunksAsync();
-			rnd = new Random(System.DateTime.Now.Millisecond + System.DateTime.Now.Minute);
+            Client = new Discord.DiscordClient();
+			var kiteDunk = new KiteBot.KiteDunk();
 
 			//Display all log messages in the console
-			client.LogMessage += (s, e) => Console.WriteLine("[{"+e.Severity+"}] {"+e.Source+"}: {"+e.Message+"}");
+			Client.LogMessage += (s, e) => Console.WriteLine("[{"+e.Severity+"}] {"+e.Source+"}: {"+e.Message+"}");
 
 			//Echo back any message received, provided it didn't come from the bot itself
-			client.MessageReceived += async (s, e) =>
+			Client.MessageReceived += async (s, e) =>
 			{
-				if (!e.Message.IsAuthor && 0 <= e.Message.Text.IndexOf("GetDunked "))
+				if (!e.Message.IsAuthor && 0 <= e.Message.Text.IndexOf("GetDunked"))
 				{
-					await client.SendMessage(e.Channel, "http://i.imgur.com/QhcNUWo.gifv");
+					await Client.SendMessage(e.Channel, "http://i.imgur.com/QhcNUWo.gifv");
 				}
 				if (!e.Message.IsAuthor && e.Message.Text.StartsWith("@KiteBot"))
 				{
-					if (e.Message.Text.StartsWith("@KiteBot #420") || e.Message.Text.ToLower().StartsWith("@KiteBot #blaze") || 0 <= e.Message.Text.ToLower().IndexOf("waifu",0))
+					if (e.Message.Text.StartsWith("@KiteBot #420") || e.Message.Text.ToLower().StartsWith("@KiteBot #blaze") ||
+					    0 <= e.Message.Text.ToLower().IndexOf("waifu", 0))
 					{
-						await client.SendMessage(e.Channel, "http://420.moe/");
+						await Client.SendMessage(e.Channel, "http://420.moe/");
 					}
 					else if (0 <= e.Message.Text.ToLower().IndexOf("help", 5))
 					{
 						var nl = System.Environment.NewLine;
-						await client.SendMessage(e.Channel, "Current Commands are:"+ nl +"#420"+ nl + "google"+ nl + "youtube" + nl + "kitedunk" + nl + "help");
+						await Client.SendMessage(e.Channel, "Current Commands are:" + nl + "#420" + nl + "google" + nl + "youtube" + nl + "kitedunk" + nl + "help");
 					}
-					else if (0 <= e.Message.Text.ToLower().IndexOf("google", 0)) 
+					else if (0 <= e.Message.Text.ToLower().IndexOf("google", 0))
 					{
-						await client.SendMessage(e.Channel, "http://lmgtfy.com/?q=" + e.Message.Text.ToLower().Substring(16).Replace(' ','+'));
+						await Client.SendMessage(e.Channel, "http://lmgtfy.com/?q=" + e.Message.Text.ToLower().Substring(16).Replace(' ', '+'));
 					}
 					else if (0 <= e.Message.Text.ToLower().IndexOf("youtube", 0))
 					{
-						await client.SendMessage(e.Channel, "https://www.youtube.com/results?search_query=" + e.Message.Text.ToLower().Substring(17).Replace(' ','+'));
+						await Client.SendMessage(e.Channel, "https://www.youtube.com/results?search_query=" + e.Message.Text.ToLower().Substring(17).Replace(' ', '+'));
 					}
 					else if (0 <= e.Message.Text.ToLower().IndexOf("dunk", 0))
 					{
-						await client.SendMessage(e.Channel, KiteDunk());
+						await Client.SendMessage(e.Channel, kiteDunk.GetRandomKiteDunk());
 					}
 					else
 					{
-						await client.SendMessage(e.Channel, "KiteBot ver. 0.2a-PreAlpha \" MOKE WEED EVRY DAY\"");
-					}					
+						await Client.SendMessage(e.Channel, "KiteBot ver. 0.3-PreAlpha");
+					}
 				}
 			};
 
 			//Convert our sync method to an async one and block the Main function until the bot disconnects
-			client.Run(async () =>
+			Client.Run(async () =>
 			{
 				//Connect to the Discord server using our email and password
-				await client.Connect(KiteBot.Properties.auth.Default.email, KiteBot.Properties.auth.Default.password);
+				await Client.Connect(KiteBot.Properties.auth.Default.email, KiteBot.Properties.auth.Default.password);
 			});
         }
-		static void GetKiteDunksAsync() 
-		{
-			string[] lines = System.IO.File.ReadAllLines(@"C:\Users\sindr\Documents\visual studio 2013\Projects\ConsoleApplication1\ConsoleApplication1\KiteDunks2.txt");
-			kiteDunks = lines;
-		}
-
-		private static string KiteDunk() {
-			int i = rnd.Next(kiteDunks.Length/2)*2;
-			string s = "\"" + kiteDunks[i+1] + "\" - " + kiteDunks[i];
-			return s;
-		}
-		//Not currently used
-		private static string GetPlainTextFromHtml(string htmlString)
-		{
-			string htmlTagPattern = "<.*?>";
-			var regexCss = new Regex("(\\<script(.+?)\\</script\\>)|(\\<style(.+?)\\</style\\>)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
-			htmlString = regexCss.Replace(htmlString, string.Empty);
-			htmlString = Regex.Replace(htmlString, htmlTagPattern, string.Empty);
-			htmlString = Regex.Replace(htmlString, @"^\s+$[\r\n]*", "", RegexOptions.Multiline);
-			htmlString = htmlString.Replace("&nbsp;", string.Empty);
-
-			return htmlString;
-		}
     }
 }
