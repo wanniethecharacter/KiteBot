6edbb0bc2cbf6162b45de6b33e1111575609995e
Josh Bennett <joshy09090@yahoo.com>
2016-01-26 15:33:18 -0700

    Character class created. Moved some routines away from constant xml parsing into Character class data

---
+++ ConsoleApplication1/Character.cs
@@ -0,0 +1,55 @@
+﻿using System.Xml.Linq;
+
+namespace KiteBot   
+{
+    public struct MapCoords
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+
+    class Character
+    {
+        public string Name { get; private set; }
+        public long UserId { get; private set; }
+        public int Level { get; private set; }
+
+        private MapCoords mapCoords = new MapCoords();
+
+        public Character (int xCoord, int yCoord, string name, long userId, int level)
+        {
+            mapCoords.x = xCoord;
+            mapCoords.y = yCoord;
+
+            Name = name;
+            UserId = userId;
+            Level = level;
+        }
+
+        public int PosX()
+        {
+            return mapCoords.x;
+        }
+
+        public int PosY()
+        {
+            return mapCoords.y;
+        }
+
+        /// <summary>
+        /// Method to move a character to a new map coordinate</summary>
+        /// <param name="xChange"> Change in x coordinate</param>
+        /// <param name="yChange"> Change in y coordinate</param>
+        /// <param name="tmpElement"> XML element reference for this character's saved data to write new map coordinates</param>
+
+        public void Move(int xChange, int yChange, XElement tmpElement)
+        {
+            mapCoords.x += xChange;
+            mapCoords.y += yChange;
+
+            tmpElement.Element("x").Value = mapCoords.x.ToString();
+            tmpElement.Element("y").Value = mapCoords.y.ToString();
+        }
+
+    }
+}
--- ConsoleApplication1/KitCoGame.cs
+++ ConsoleApplication1/KitCoGame.cs
@@ -11,20 +11,34 @@ namespace KiteBot
     {
         private XDocument characterData;
         private XDocument mapData;
-        public static string DataDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName;
-        public static string CharacterData = DataDirectory + "\\CharacterData.xml";
-        public static string MapData = DataDirectory + "\\RoomData.xml";
+        private static string DataDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName;
+        private static string CharacterData = DataDirectory + "\\CharacterData.xml";
+        private static string MapData = DataDirectory + "\\RoomData.xml";
 
         static int maxWidth = 5;
         static int maxHeight = 5;
 
+        //holds a user ID referenceable list of characters
+        static Dictionary<long, Character> characters = new Dictionary<long, Character>();
+
         MapCell[,] map = new MapCell[maxWidth, maxHeight];
 
+        //constructor
         public KitCoGame()
         {
+            //load saved data
             characterData = XDocument.Load(CharacterData);
             mapData = XDocument.Load(MapData);
+            
+            //Build character Dictionary
+            List<XElement> characterDataList = new List<XElement>();
+            characterDataList.AddRange(characterData.Descendants("character"));
+            foreach (XElement e in characterDataList)
+            {
+                AddNewCharacter(e);
+            }
 
+            //Build rooms map
             List<XElement> roomList = new List<XElement>();
             roomList.AddRange(mapData.Descendants("room"));
 
@@ -51,118 +65,138 @@ namespace KiteBot
             }
         }
 
+        //Public method for returning proper string messages saved in room data
         public string GetGameResponse(Message message)
         {
             string userIdString = message.User.Id.ToString();
-            List<XElement> characters = new List<XElement>();
-            characters.AddRange(characterData.Descendants("character"));
+            List<XElement> characterList = new List<XElement>();
+            characterList.AddRange(characterData.Descendants("character"));
 
-            if ( !message.IsAuthor && message.Text.StartsWith("@KiteBot") )
+            XElement tmpXElement = null;
+
+            if (!message.IsAuthor && message.Text.StartsWith("@KiteBot"))
             {
-                foreach(XElement e in characters)
+                if (characters.ContainsKey(message.User.Id))
                 {
-                    if (e.Element("userid").Value == userIdString)
+                    Character currentCharacter = characters[message.User.Id];
+                   
+                    foreach(XElement e in characterList)
                     {
-                        //look commands
-                        if (0 <= message.Text.ToLower().IndexOf("look"))
+                        if (Int64.Parse(e.Element("userid").Value) == currentCharacter.UserId)
                         {
-                            foreach (string s in map[Int32.Parse(e.Element("x").Value), Int32.Parse(e.Element("y").Value)].objectDictionary.Keys)
-                            {
-                                if (0 <= message.Text.ToLower().IndexOf(s.ToLower() ))
-                                {
-                                    return e.Element("name").Value + " " + map[Int32.Parse(e.Element("x").Value), Int32.Parse(e.Element("y").Value)].objectDictionary[s];
-                                }
-                            }
-
-                            foreach (string s in map[Int32.Parse(e.Element("x").Value), Int32.Parse(e.Element("y").Value)].directions.Keys)
-                            {
-                                if (0 <= message.Text.ToLower().IndexOf(s.ToLower()))
-                                {
-                                    return e.Element("name").Value + " " + map[Int32.Parse(e.Element("x").Value), Int32.Parse(e.Element("y").Value)].directions[s];
-                                }
-                            }
-
-                            return e.Element("name").Value + " " + map[Int32.Parse(e.Element("x").Value), Int32.Parse(e.Element("y").Value)].roomDesc;
+                            tmpXElement = e;
+                            break;
                         }
-
-
-                        //move commands
-                        else if (0 <= message.Text.ToLower().IndexOf("move"))
-                        {
-                            int x = Int32.Parse(e.Element("x").Value);
-                            int y = Int32.Parse(e.Element("y").Value);
-
-                            if (0 <= message.Text.ToLower().IndexOf("north"))
-                            {
-                                if (y + 1 >= maxHeight)
-                                    return "Cannot go that way";
-                                else
-                                {
-                                    y++;
-                                    e.Element("y").Value = y.ToString();
-                                    characterData.Save(CharacterData);
-                                    return e.Element("name").Value + " You move to the north. " + map[Int32.Parse(e.Element("x").Value), Int32.Parse(e.Element("y").Value)].roomDesc;
-                                }
-                            }
-
-                            else if (0 <= message.Text.ToLower().IndexOf("south"))
-                            {
-                                if (y - 1 < 0)
-                                    return "Cannot go that way";
-                                else
-                                {
-                                    y--;
-                                    e.Element("y").Value = y.ToString();
-                                    characterData.Save(CharacterData);
-                                    return e.Element("name").Value + " You move to the south. " + map[Int32.Parse(e.Element("x").Value), Int32.Parse(e.Element("y").Value)].roomDesc;
-                                }
-                            }
-
-                            else if (0 <= message.Text.ToLower().IndexOf("east"))
-                            {
-                                if (x + 1 >= maxWidth)
-                                    return "Cannot go that way";
-                                else
-                                {
-                                    x++;
-                                    e.Element("x").Value = x.ToString();
-                                    characterData.Save(CharacterData);
-                                    return e.Element("name").Value + " You move to the east. " + map[Int32.Parse(e.Element("x").Value), Int32.Parse(e.Element("y").Value)].roomDesc;
-                                }
-                            }
-
-                            else if (0 <= message.Text.ToLower().IndexOf("west"))
-                            {
-                                if (x - 1 < 0)
-                                    return "Cannot go that way";
-                                else
-                                {
-                                    x--;
-                                    e.Element("x").Value = x.ToString();
-                                    characterData.Save(CharacterData);
-                                    return e.Element("name").Value + " You move to the west. " + map[Int32.Parse(e.Element("x").Value), Int32.Parse(e.Element("y").Value)].roomDesc;
-                                }
-                            }
-                        }
-
-                        else
-                            return "Welcome Back " + e.Element("name").Value + ". Current commands are look (object or direction) and move(direction)";
                     }
+
+                    //look commands
+                    if (0 <= message.Text.ToLower().IndexOf("look"))
+                    {
+                        foreach (string s in map[currentCharacter.PosX(), currentCharacter.PosY()].objectDictionary.Keys)
+                        {
+                            if (0 <= message.Text.ToLower().IndexOf(s.ToLower()))
+                            {
+                                return currentCharacter.Name + " " + map[currentCharacter.PosX(), currentCharacter.PosY()].objectDictionary[s];
+                            }
+                        }
+
+                        foreach (string s in map[currentCharacter.PosX(), currentCharacter.PosY()].directions.Keys)
+                        {
+                            if (0 <= message.Text.ToLower().IndexOf(s.ToLower()))
+                            {
+                                return currentCharacter.Name + " " + map[currentCharacter.PosX(), currentCharacter.PosY()].directions[s];
+                            }
+                        }
+
+                        return currentCharacter.Name + " " + map[currentCharacter.PosX(), currentCharacter.PosY()].roomDesc;
+                    }
+
+
+                    //move commands
+                    else if (0 <= message.Text.ToLower().IndexOf("move"))
+                    {
+                        int x = currentCharacter.PosX();
+                        int y = currentCharacter.PosY();
+
+                        if (0 <= message.Text.ToLower().IndexOf("north"))
+                        {
+                            if (y + 1 >= maxHeight)
+                                return "Cannot go that way";
+                            else
+                            {
+                                currentCharacter.Move(0,1, tmpXElement);
+                                characterData.Save(CharacterData);
+                                return currentCharacter.Name + " You move to the north. " + map[currentCharacter.PosX(), currentCharacter.PosY()].roomDesc;
+                            }
+                        }
+
+                        else if (0 <= message.Text.ToLower().IndexOf("south"))
+                        {
+                            if (y - 1 < 0)
+                                return "Cannot go that way";
+                            else
+                            {
+                                currentCharacter.Move(0, -1, tmpXElement);
+                                characterData.Save(CharacterData);
+                                return currentCharacter.Name + " You move to the south. " + map[currentCharacter.PosX(), currentCharacter.PosY()].roomDesc;
+                            }
+                        }
+
+                        else if (0 <= message.Text.ToLower().IndexOf("east"))
+                        {
+                            if (x + 1 >= maxWidth)
+                                return "Cannot go that way";
+                            else
+                            {
+                                currentCharacter.Move(1, 0, tmpXElement);
+                                characterData.Save(CharacterData);
+                                return currentCharacter.Name + " You move to the east. " + map[currentCharacter.PosX(), currentCharacter.PosY()].roomDesc;
+                            }
+                        }
+
+                        else if (0 <= message.Text.ToLower().IndexOf("west"))
+                        {
+                            if (x - 1 < 0)
+                                return "Cannot go that way";
+                            else
+                            {
+                                currentCharacter.Move(-1, 0, tmpXElement);
+                                characterData.Save(CharacterData);
+                                return currentCharacter.Name + " You move to the west. " + map[currentCharacter.PosX(), currentCharacter.PosY()].roomDesc;
+                            }
+                        }
+                    }
+
+                    else
+                        return "Welcome Back " + currentCharacter.Name + ". Current commands are look (object or direction) and move(direction)";
                 }
 
-                XElement newCharacter = new XElement("character",
-                                                       new XElement("userid", message.User.Id),
-                                                       new XElement("name", message.User.Name),
-                                                       new XElement("level", 0),
-                                                       new XElement("x", 2),
-                                                       new XElement("y", 0));
-                characterData.Element("chardata").Add(newCharacter);
-                characterData.Save(CharacterData);
+                else
+                {
+                    XElement newCharacter = new XElement("character",
+                                                           new XElement("userid", message.User.Id),
+                                                           new XElement("name", message.User.Name),
+                                                           new XElement("level", 0),
+                                                           new XElement("x", 2),
+                                                           new XElement("y", 0));
+                    characterData.Element("chardata").Add(newCharacter);
+                    characterData.Save(CharacterData);
+                    AddNewCharacter(newCharacter);
 
-                return "Hi there, I haven't seen you before. Added new user: " + newCharacter.Element("name").Value.ToString();
+                    return "Hi there, I haven't seen you before. Added new user: " + newCharacter.Element("name").Value.ToString();
+                }
+
             }
+            return null;
+        }
 
-            else return null;
+        private void AddNewCharacter(XElement e)
+        {
+            characters.Add(Int64.Parse(e.Element("userid").Value), new Character(Int32.Parse(e.Element("x").Value),
+                                                                                 Int32.Parse(e.Element("y").Value),
+                                                                                 e.Element("name").Value,
+                                                                                 Int64.Parse(e.Element("userid").Value),
+                                                                                 Int32.Parse(e.Element("level").Value)));
         }
 
     }
