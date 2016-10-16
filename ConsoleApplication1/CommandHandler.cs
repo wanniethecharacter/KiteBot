7b2f7a2d83a6091ba596fabfcfb0406329039cba
Sindre G. Langhus <sindre_langhus@msn.com>
2016-10-16 15:48:24 +0200

    First stage converting to final commandhandler

---
+++ ConsoleApplication1/CommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace KiteBot
+{
+    public class CommandHandler
+    {
+        private CommandService _commands;
+        private DiscordSocketClient _client;
+        private IDependencyMap _map;
+
+        public async Task Install(IDependencyMap map)
+        {
+            // Create Command Service, inject it into Dependency Map
+            _client = map.Get<DiscordSocketClient>();
+            _commands = new CommandService();
+            map.Add(_commands);
+            _map = map;
+
+            await _commands.AddModules(Assembly.GetEntryAssembly(), _map);
+
+            _client.MessageReceived += HandleCommand;
+        }
+
+        public async Task HandleCommand(SocketMessage parameterMessage)
+        {
+            // Don't handle the command if it is a system message
+            var message = parameterMessage as SocketUserMessage;
+            if (message == null) return;
+
+            // Mark where the prefix ends and the command begins
+            int argPos = 0;
+            // Determine if the message has a valid prefix, adjust argPos 
+            if (!(message.HasMentionPrefix(_client.CurrentUser, ref argPos) || message.HasCharPrefix('!', ref argPos))) return;
+
+            // Create a Command Context
+            var context = new CommandContext(_client, message);
+            // Execute the Command, store the result
+            var result = await _commands.Execute(context, argPos, _map);
+
+            // If the command failed, notify the user
+            if (!result.IsSuccess)
+                await message.Channel.SendMessageAsync($"**Error:** {result.ErrorReason}");
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -13,56 +13,19 @@ namespace KiteBot
 {
     class Program
     {
-
-//#region Trap application termination
-//        [DllImport("Kernel32")]
-//        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
-
-//        private delegate bool EventHandler(CtrlType sig);
-//        static EventHandler _handler;
-
-//        enum CtrlType
-//        {
-//            CtrlCEvent = 0,
-//            CtrlBreakEvent = 1,
-//            CtrlCloseEvent = 2,
-//            CtrlLogoffEvent = 5,
-//            CtrlShutdownEvent = 6
-//        }
-
-//        private static bool Handler(CtrlType sig)
-//        {
-//            Console.WriteLine("Exiting system due to external CTRL-C, or process kill, or shutdown");
-
-//            //cleanup here
-//            KiteChat.MultiDeepMarkovChains.Save();
-
-//            Console.WriteLine("Cleanup complete");
-
-//            //allow main to run off
-//            _exitSystem = false;
-
-//            //shutdown right away so there are no lingering threads
-//            Environment.Exit(-1);
-
-//            return true;
-//        }
-//#endregion
-
         public static DiscordSocketClient Client;
         public static CommandService CommandService = new CommandService();
         public static JsonSettings Settings;
         private static KiteChat _kiteChat;
         public static string ContentDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent?.Parent?.FullName;
         private static string SettingsPath => ContentDirectory + "/Content/settings.json";
+        private static CommandHandler _handler;
 
-        private static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+        private static void Main(string[] args) => AsyncMain(args).GetAwaiter().GetResult();
 
-        public async Task Start()
+        public static async Task AsyncMain(string[] args)
         {
-            //_handler += Handler;
-            //SetConsoleCtrlHandler(_handler, true);
-            Client = new DiscordSocketClient(new DiscordSocketConfig()
+            Client = new DiscordSocketClient(new DiscordSocketConfig
             {
                 LogLevel = LogSeverity.Info,
                 MessageCacheSize = 0                
@@ -90,7 +53,7 @@ namespace KiteBot
                 return Task.CompletedTask;
             };
 
-            Client.MessageReceived += async (msg) =>
+            Client.MessageReceived += async msg =>
             {
                 try
                 {
@@ -104,7 +67,7 @@ namespace KiteBot
                 }
             }; 
 
-            Client.GuildAvailable += async (server) =>
+            Client.GuildAvailable += async server =>
             {
                 if (Client.GetGuilds().Any())
                 {
@@ -112,7 +75,7 @@ namespace KiteBot
                 }
             };
             
-            Client.JoinedGuild += (server) =>
+            Client.JoinedGuild += server =>
             {
                 Console.WriteLine("Connected to " + server.Name);
                 return Task.CompletedTask;
@@ -152,48 +115,16 @@ namespace KiteBot
                 }
             };
 
-            await InstallCommands();
-
             await Client.LoginAsync(TokenType.Bot, Settings.DiscordToken);
-            // Connect the client to Discord's gateway
             await Client.ConnectAsync();
-            
+
+            var map = new DependencyMap();
+            map.Add(Client);
+
+            _handler = new CommandHandler();
+            await _handler.Install(map);
+
             await Task.Delay(-1);
-        }
-        [Obsolete]
-        public static void RssFeedSendMessage(object s, Feed.UpdatedFeedEventArgs e)
-        {
-            var channel = (ITextChannel)Client.GetChannel(85842104034541568);
-
-            channel.SendMessageAsync(e.Title + " live now at GiantBomb.com\r\n" + e.Link);
-	    }
-
-        public async Task InstallCommands()
-        {
-            // Hook the MessageReceived Event into our Command Handler
-            Client.MessageReceived += HandleCommand;
-            // Discover all of the commands in this assembly and load them.
-            await CommandService.LoadAssembly(Assembly.GetEntryAssembly());
-        }
-
-        public async Task HandleCommand(IMessage paramMessage)
-        {
-            // Cast paramMessage to an IUserMessage, return if the message was a System message.
-            var msg = paramMessage as IUserMessage;
-            if (msg == null) return;
-            // Internal integer, marks where the command begins
-            int argPos = 0;
-            // Get the current user (used for Mention parsing)
-            var currentUser = await Client.GetCurrentUserAsync();
-            // Determine if the message is a command, based on if it starts with '!' or a mention prefix
-            if (msg.HasCharPrefix('~', ref argPos) || msg.HasMentionPrefix(currentUser, ref argPos))
-            {
-                // Execute the command. (result does not indicate a return value, 
-                // rather an object stating if the command executed succesfully)
-                var result = await CommandService.Execute(msg, argPos);
-                if (!result.IsSuccess)
-                    await msg.Channel.SendMessageAsync(result.ErrorReason);
-            }
         }
 
         public struct JsonSettings
